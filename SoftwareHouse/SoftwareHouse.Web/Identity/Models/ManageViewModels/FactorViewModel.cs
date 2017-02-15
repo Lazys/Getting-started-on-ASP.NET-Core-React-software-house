0d82740b95dd7debe76b2a6c417cd8e483d019cd
Przemek Smyrdek <przemek.smyrdek@gmail.com>
2017-02-15 15:25:14 +0100

    Episode 02 - Introduction / Back-end (#1)
    * gitIgnore
    
    * Solution init
    
    * connection string
    
    * Identity models and services moved to identity namespace
    
    * ApplicationUser model moved to data -> models namespace
    
    * Added missing namespace

---
+++ SoftwareHouse/SoftwareHouse.Web/Identity/Models/ManageViewModels/FactorViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SoftwareHouse.Web.Identity.Models.ManageViewModels
+{
+    public class FactorViewModel
+    {
+        public string Purpose { get; set; }
+    }
+}
