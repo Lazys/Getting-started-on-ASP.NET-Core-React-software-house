ca734cd8cdfa317bddffe20dc9101950218f423d
Adrian Bystrek <adrian.bystrek@gmail.com>
2017-02-27 20:10:12 +0100

    Added projects service & project dto

---
+++ SoftwareHouse/SoftwareHouse.Services/Services/ProjectsService.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using SoftwareHouse.Services.DataContracts;
+using SoftwareHouse.Services.Interfaces;
+using SoftwareHouse.DataAccess.Interfaces;
+using System.Linq;
+
+namespace SoftwareHouse.Services.Services
+{
+    public class ProjectsService : IProjectsService
+    {
+        private readonly IProjectsRepository _projectsRepository;
+
+        public ProjectsService(IProjectsRepository projectsRepository)
+        {
+            _projectsRepository = projectsRepository;
+        }
+
+        public List<ProjectDto> GetAll()
+        {
+            return _projectsRepository.GetAll()
+                                      .Select(x => new ProjectDto
+                                      {
+                                          Id = x.Id,
+                                          Name = x.Name,
+                                          Description = x.Description,
+                                          IsDeleted = x.IsDeleted,
+                                          CreationDate = x.CreationDate
+                                      })
+                                      .ToList();
+        }
+    }
+}
