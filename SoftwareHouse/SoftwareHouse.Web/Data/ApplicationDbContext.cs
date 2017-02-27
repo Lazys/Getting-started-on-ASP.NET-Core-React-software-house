3161733fed1e2a25ca4d26a0a35eb525847e05e7
Adrian Bystrek <adrian.bystrek@gmail.com>
2017-02-27 18:32:22 +0100

    Added project entity

--- SoftwareHouse/SoftwareHouse.Web/Data/ApplicationDbContext.cs
+++ SoftwareHouse/SoftwareHouse.Web/Data/ApplicationDbContext.cs
@@ -1,16 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using SoftwareHouse.Web.Identity.Models;
 using SoftwareHouse.Web.Data.Models;
 
 namespace SoftwareHouse.Web.Data
 {
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        public DbSet<Project> Projects { get; set; }
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
@@ -22,6 +19,8 @@ namespace SoftwareHouse.Web.Data
             // Customize the ASP.NET Identity model and override the defaults if needed.
             // For example, you can rename the ASP.NET Identity table names and more.
             // Add your customizations after calling base.OnModelCreating(builder);
+
+            builder.Entity<Project>().HasKey(x => x.Id);
         }
     }
 }
