3cd99b133b6e47a97eb1bf6753a8747b53f58a57
Adrian Bystrek <adrian.bystrek@gmail.com>
2017-02-28 21:38:22 +0100

    Projects name required

--- SoftwareHouse/SoftwareHouse.DataAccess/Migrations/20170228203651_ProjectsNameRequired.Designer.cs
+++ SoftwareHouse/SoftwareHouse.DataAccess/Migrations/20170228203651_ProjectsNameRequired.Designer.cs
@@ -8,9 +8,10 @@ using SoftwareHouse.DataAccess;
 namespace SoftwareHouse.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20170228203651_ProjectsNameRequired")]
+    partial class ProjectsNameRequired
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.1.0-rtm-22752")
