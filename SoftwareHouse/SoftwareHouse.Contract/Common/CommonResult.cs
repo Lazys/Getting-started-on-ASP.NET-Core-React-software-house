89f24d0c8f2fe58c8cdc2f13b028922d0f345d9b
Adrian Bystrek <adrian.bystrek@gmail.com>
2017-02-28 21:01:31 +0100

    Common result

---
+++ SoftwareHouse/SoftwareHouse.Contract/Common/CommonResult.cs
@@ -0,0 +1,53 @@
+﻿namespace SoftwareHouse.Contract.Common
+{
+    public class CommonResult
+    {
+        public bool IsSuccess { get; set; }
+        public string ErrorMessage { get; set; }
+
+        protected CommonResult()
+        {
+            IsSuccess = true;
+        }
+
+        protected CommonResult(string errorMessage)
+        {
+            IsSuccess = false;
+            ErrorMessage = errorMessage;
+        }
+
+        public static CommonResult Success()
+        {
+            return new CommonResult();
+        }
+
+        public static CommonResult Failure(string errorMessage)
+        {
+            return new CommonResult(errorMessage);
+        }
+    }
+
+    public class CommonResult<T> : CommonResult
+    {
+        public T Item { get; set; }
+
+        public CommonResult(T item) : base()
+        {
+            Item = item;
+        }
+
+        protected CommonResult(string errorMessage) : base(errorMessage)
+        {
+        }
+
+        public static CommonResult<T> Success<T>(T item)
+        {
+            return new CommonResult<T>(item);
+        }
+
+        public static CommonResult<T> Failure<T>(string errorMessage)
+        {
+            return new CommonResult<T>(errorMessage);
+        }
+    }
+}
